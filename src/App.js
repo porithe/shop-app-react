834940cd2b9493760f524e1fb834662275159468
porithe <malkowski.buisness@gmail.com>
2019-06-06 10:38:34 +0200

    base with nodemodules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import styled from 'styled-components';
 
-function App() {
-  return (
+
+const App = () => (
     <div className="App">
 
     </div>
-  );
-}
+);
 
 export default App;
